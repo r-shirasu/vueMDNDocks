9b19fb73104e972dbce9c8899fab48a60a080631
r-shirasu <risako.shirasu@asia-quest.jp>
2022-03-08 00:18:04 +0900

    feat :adding a new todo form

---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  methods: {
+    onSubmit() {
+      if (this.label === "") {
+        return;
+      }
+      this.$emit("todo-added", this.label);
+      this.label = "";
+    },
+  },
+  data() {
+    return {
+      label: "",
+    };
+  },
+};
+</script>
+
+<template>
+  <form @submit.prevent="onSubmit">
+    <label for="new-todo-input"> What needs to be done? </label>
+    <input
+      type="text"
+      id="new-todo-input"
+      name="new-todo"
+      autocomplete="off"
+      v-model="label"
+    />
+    <button type="submit">Add</button>
+  </form>
+</template>
